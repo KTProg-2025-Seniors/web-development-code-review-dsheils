804353aa5749953c49511b24728ca6cc7b26d2d7
dsheils <dsheils2025@jpkeefehs.org>
2024-09-13 21:49:07 -0400

    Added currency converter

--- script.js
+++ script.js
@@ -39,3 +39,18 @@ function sinTime(){
       
       document.getElementById("sintime").innerHTML = formatter.format(new Date());
 }
+
+function convert(currency){
+  var usd = document.getElementById("USD")
+  console.log(currency);
+  if(currency == 'singapore'){
+    document.getElementById("SGD").value = usd.value * 1.3;
+  }
+  else if(currency == "australia"){
+    console.log("aus sent");
+    document.getElementById("aud").value = usd.value * 1.49;
+  }
+  else if(currency == 'iceland'){
+    document.getElementById("isk").value = usd.value * 137.5;
+  }
+}
